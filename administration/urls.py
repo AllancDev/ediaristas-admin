e06a568b3486dbef6c85dc90ad55645db07452b8
Allan Camargo <allan.cdev@gmail.com>
2024-02-02 05:11:01 -0300

    feat: add list user

--- administration/urls.py
+++ administration/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('services/register', service_views.register_service, name="register_service"),
     path('services/list', service_views.list_services, name='list_services'),
     path('services/edit/<int:id>', service_views.edit_services, name='edit_services'),
-    path('users/register', user_views.register_user, name='register_user')
+    path('users/register', user_views.register_user, name='register_user'),
+    path('users/list', user_views.list_users, name='list_users'),
 ]
--- administration/views/user_views.py
+++ administration/views/user_views.py
@@ -1,5 +1,6 @@
 from ..forms.user_forms import UserForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
 
 
 def register_user(request):
@@ -8,7 +9,15 @@ def register_user(request):
 
         if user_form.is_valid():
             user_form.save()
+
+            return redirect('list_users')
     else:
         user_form = UserForm()
 
     return render(request, 'users/user_form.html', {'form_user': user_form})
+
+def list_users(request):
+    User = get_user_model()
+    users = User.objects.filter(is_superuser=True)
+
+    return render(request, 'users/user_list.html', {'users': users})
